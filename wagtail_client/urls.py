466340b1b1ec00b25f57e649a8c9ceca21ff4c0a
cobusc <cobus.carstens@gmail.com>
2018-04-05 10:47:57 +0200

    Added example of a protected page which automatically redirects to the Identity Provider for login, and then redirects back to the protected page. This will work in general.

--- wagtail_client/urls.py
+++ wagtail_client/urls.py
@@ -13,25 +13,25 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.views.generic.base import TemplateView
 from django.contrib.auth.views import logout
+from django.views.generic import RedirectView
 
 from wagtail.wagtailadmin import urls as wagtailadmin_urls
 from wagtail.wagtaildocs import urls as wagtaildocs_urls
 from wagtail.wagtailcore import urls as wagtail_urls
 
-from wagtail_client.views import HomePageView
+from wagtail_client.views import HomePageView, ProtectedPageView
 
 urlpatterns = [
+    url(r'^admin/login/', RedirectView.as_view(pattern_name="oidc_authentication_init")),
     url(r'^admin/', admin.site.urls),
     url(r'^cms/', include(wagtailadmin_urls)),
     url(r'^documents/', include(wagtaildocs_urls)),
     url(r'^pages/', include(wagtail_urls)),
     url(r'^oidc/', include('mozilla_django_oidc.urls')),
+    url(r'^protected/', ProtectedPageView.as_view(), name="login"),
     url(r'^login/', HomePageView.as_view(), name="login"),
     url(r"^$", HomePageView.as_view(), name="home"),
     url(r"^logout/", logout, name="logout"),
--- wagtail_client/views.py
+++ wagtail_client/views.py
@@ -1,4 +1,6 @@
 from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView
 
 
@@ -10,5 +12,18 @@ class HomePageView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(HomePageView, self).get_context_data(**kwargs)
+        context["settings"] = settings
+        return context
+
+
+@method_decorator(login_required, name="dispatch")
+class ProtectedPageView(TemplateView):
+    """
+    A protected page view that with some customisable features.
+    """
+    template_name = "wagtail_client/protected.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(ProtectedPageView, self).get_context_data(**kwargs)
         context["settings"] = settings
         return context
