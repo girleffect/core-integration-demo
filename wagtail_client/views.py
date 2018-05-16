35d446a805efe6c715c78a6cde7cb1fd568af082
cobusc <cobus.carstens@gmail.com>
2018-05-16 14:20:42 +0200

    Redirects from login pages needs the query string included

--- wagtail_client/views.py
+++ wagtail_client/views.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 
 
 class HomePageView(TemplateView):
@@ -27,3 +27,7 @@ class ProtectedPageView(TemplateView):
         context = super(ProtectedPageView, self).get_context_data(**kwargs)
         context["settings"] = settings
         return context
+
+
+class RedirectWithQueryStringView(RedirectView):
+    query_string = True
